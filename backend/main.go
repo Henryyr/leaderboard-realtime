45e173f4a60b4ed30050e6486847610c3998bd7e
henryyr <henry.360420@gmail.com>
2025-06-26 00:53:43 +0800

    test

--- backend/main.go
+++ backend/main.go
@@ -6,9 +6,12 @@ import (
 	ws "leaderboard-realtime/websocket"
 
 	"log"
+	"net/http"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/filesystem"
 	"github.com/gofiber/websocket/v2"
 )
 
@@ -17,6 +20,20 @@ func main() {
 
 	app.Use(cors.New())
 
+	// Serve static files from ../public
+	app.Use("/", filesystem.New(filesystem.Config{
+		Root:   http.Dir("../public"),
+		Browse: true,
+		Index:  "index.html",
+		MaxAge: 3600,
+	}))
+
+	// Connect to Redis using REDIS_URL env variable
+	redisURL := os.Getenv("REDIS_URL")
+	if redisURL != "" {
+		// override config.ConnectRedis to use redisURL if present
+		os.Setenv("REDIS_URL", redisURL)
+	}
 	config.ConnectRedis()
 
 	app.Post("/submit", handler.SubmitScore)
